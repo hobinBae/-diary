bb42f0c456bd142a7a9fd2d219363ada495b1f22
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-05-27 01:23:48 +0900

    초기세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import { getEmotionImage } from "./util/get_emotion-image";
 import "./App.css";
 
 function App() {
-  return()
+  return (
+    <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
